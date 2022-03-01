e2790dda570ba341013df12b03d3856ab59b5c70
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 23:38:33 +0100

    made use of create method from user service

--- src/main/java/io/rently/userservice/controller/UserController.java
+++ src/main/java/io/rently/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package io.rently.userservice.controller;
 
 import io.rently.userservice.dto.ResponseBody;
+import io.rently.userservice.dto.User;
 import io.rently.userservice.service.UserService;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.*;
@@ -9,29 +10,29 @@ import java.sql.Timestamp;
 
 @RestController
 public class UserController implements ErrorController {
-    public static final String PREFIX = "api/v1";
+    public static final String BASE_ROUTE = "api/v1";
 
-    @GetMapping(value = PREFIX + "/users")
+    @GetMapping(value = BASE_ROUTE + "/users")
     public static ResponseBody getUsers() {
         return UserService.getUsers();
     }
 
-    @GetMapping(value = PREFIX + "/users/{id}")
+    @GetMapping(value = BASE_ROUTE + "/users/{id}")
     public static ResponseBody getUser(@PathVariable String id) {
         return UserService.getUserById(id);
     }
 
-    @PostMapping(value = PREFIX + "/users")
-    public static ResponseBody addUser() {
-        return UserService.addUser();
+    @PostMapping(value = BASE_ROUTE + "/users")
+    public static ResponseBody addUser(@RequestBody User user) {
+        return UserService.addUser(user);
     }
 
-    @PutMapping(value = PREFIX + "/users/{id}")
+    @PutMapping(value = BASE_ROUTE + "/users/{id}")
     public static ResponseBody updatedUser(@PathVariable String id) {
         return UserService.updateUserById(id);
     }
 
-    @DeleteMapping(value = PREFIX + "/users/{id}")
+    @DeleteMapping(value = BASE_ROUTE + "/users/{id}")
     public static ResponseBody deleteUser(@PathVariable String id) {
         return UserService.deleteUserById(id);
     }
@@ -39,8 +40,8 @@ public class UserController implements ErrorController {
     @RequestMapping("/error")
     public static ResponseBody handleErrorResponse() {
         return new ResponseBody
-                .Builder(new Timestamp(System.currentTimeMillis()), 500)
-                .setData("Invalid or incomplete URL path")
+                .Builder(new Timestamp(System.currentTimeMillis()), 500) // wrong
+                .setData("An error occurred")
                 .build();
     }
 }
