d57afa3501f6ee0e1f41bf109550b2ad01ada493
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 21:46:15 +0100

    used user service methods in user controller class

--- src/main/java/io/rently/userservice/controller/UserController.java
+++ src/main/java/io/rently/userservice/controller/UserController.java
@@ -1,25 +1,16 @@
 package io.rently.userservice.controller;
 
-import io.rently.userservice.dto.User;
 import io.rently.userservice.dto.ResponseBody;
+import io.rently.userservice.dto.User;
 import io.rently.userservice.service.UserService;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
-import java.util.UUID;
 
 @RestController
 public class UserController implements ErrorController {
     public static final String PREFIX = "api/v1";
-
-    @RequestMapping("/error")
-    public static ResponseBody error() {
-        return new ResponseBody
-                .Builder(new Timestamp(System.currentTimeMillis()), 500)
-                .setData("Invalid or incomplete URL path")
-                .build();
-    }
 
     @GetMapping(value = PREFIX + "/users")
     public static ResponseBody getUsers() {
@@ -27,31 +18,30 @@ public class UserController implements ErrorController {
     }
 
     @GetMapping(value = PREFIX + "/users/{id}")
-    public static ResponseBody getUser(@PathVariable(required = false) String id) {
+    public static ResponseBody getUser(@PathVariable String id) {
         return UserService.getUserById(id);
     }
 
     @PostMapping(value = PREFIX + "/users")
     public static ResponseBody addUser() {
-        return new ResponseBody
-                .Builder(new Timestamp(System.currentTimeMillis()), 201)
-                .setData("POST request")
-                .build();
+        return UserService.addUser();
     }
 
-    @PutMapping(value = PREFIX + "/users/user")
-    public static ResponseBody updatedUser() {
-        return new ResponseBody
-                .Builder(new Timestamp(System.currentTimeMillis()), 201)
-                .setData("PUT request")
-                .build();
+    @PutMapping(value = PREFIX + "/users/{id}")
+    public static ResponseBody updatedUser(@PathVariable String id) {
+        return UserService.updateUserById(id);
     }
 
-    @DeleteMapping(value = PREFIX + "/users/user")
-    public static ResponseBody deleteUser() {
+    @DeleteMapping(value = PREFIX + "/users/{id}")
+    public static ResponseBody deleteUser(@PathVariable String id) {
+        return UserService.deleteUserById(id);
+    }
+
+    @RequestMapping("/error")
+    public static ResponseBody error() {
         return new ResponseBody
-                .Builder(new Timestamp(System.currentTimeMillis()), 201)
-                .setData("DELETE request")
+                .Builder(new Timestamp(System.currentTimeMillis()), 500)
+                .setData("Invalid or incomplete URL path")
                 .build();
     }
 }
--- src/main/java/io/rently/userservice/service/UserService.java
+++ src/main/java/io/rently/userservice/service/UserService.java
@@ -4,35 +4,14 @@ import io.rently.userservice.dto.ResponseBody;
 import io.rently.userservice.dto.User;
 
 import java.sql.Timestamp;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 public class UserService {
-    private static final List<User> users = Arrays.asList(
-            new User
-                    .Builder("1")
-                    .setUsername("branlix2000")
-                    .setFullname("Noah Greff")
-                    .setEmail("something@gmail.com")
-                    .setPhone("+31 06 41 53 14")
-                    .build(),
-
-            new User
-                    .Builder(UUID.randomUUID().toString())
-                    .setUsername("than00ber")
-                    .setFullname("Chandler Greff")
-                    .setEmail("something.other@yahoo.com")
-                    .build(),
-
-            new User
-                    .Builder(UUID.randomUUID().toString())
-                    .setUsername("chew kok")
-                    .setFullname("Chew Kok")
-                    .setEmail("chew.kok@hotmail.com")
-                    .build()
-    );
+    private static final List<User> users = new ArrayList<>(Arrays.asList(
+            new User.Builder("1").setUsername("branlix2000").setFullname("Noah Greff").setEmail("something@gmail.com").setPhone("+31 06 41 53 14").build(),
+            new User.Builder("2").setUsername("than00ber").setFullname("Chandler Greff").setEmail("something.other@yahoo.com").build(),
+            new User.Builder("3").setUsername("chew kok").setFullname("Chew Kok").setEmail("chew.kok@hotmail.com").build()
+    ));
 
     public static ResponseBody getUsers() {
         return new ResponseBody
@@ -47,6 +26,50 @@ public class UserService {
                 return new ResponseBody
                         .Builder(new Timestamp(System.currentTimeMillis()), 200)
                         .setData(user)
+                        .build();
+            }
+        }
+
+        return new ResponseBody
+                .Builder(new Timestamp(System.currentTimeMillis()), 404)
+                .setData("User with id " + id + " not found")
+                .build();
+    }
+
+    public static ResponseBody addUser() {
+        return null;
+    }
+
+    public static ResponseBody deleteUserById(String id) {
+        for (User user: users.stream().toList()) {
+            if (Objects.equals(user.getId(), id)) {
+                users.remove(user);
+
+                return new ResponseBody
+                        .Builder(new Timestamp(System.currentTimeMillis()), 200)
+                        .setData("Successfully removed user with id " + id)
+                        .build();
+            }
+        }
+
+        return new ResponseBody
+                .Builder(new Timestamp(System.currentTimeMillis()), 404)
+                .setData("User with id " + id + " not found")
+                .build();
+    }
+
+    public static ResponseBody updateUserById(String id) {
+        for (User user: users.stream().toList()) {
+            if (Objects.equals(user.getId(), id)) {
+                users.remove(user);
+                users.add(new User
+                        .Builder(user.getId())
+                        .build()
+                );
+
+                return new ResponseBody
+                        .Builder(new Timestamp(System.currentTimeMillis()), 200)
+                        .setData("Successfully updated user with id " + id)
                         .build();
             }
         }
