e8f3fd44edafccac27b2fb20d204207d4881c17a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 17:34:40 +0100

    handled invalid url path

--- src/main/java/io/rently/userservice/controller/UserController.java
+++ src/main/java/io/rently/userservice/controller/UserController.java
@@ -1,7 +1,8 @@
 package io.rently.userservice.controller;
 
 import io.rently.userservice.dto.User;
-import io.rently.userservice.model.ResponseBody;
+import io.rently.userservice.dto.ResponseBody;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ import java.sql.Timestamp;
 import java.util.UUID;
 
 @RestController
-public class UserController {
-    public static final String ROUTE = "user-service/v1/users";
+public class UserController implements ErrorController {
+    public static final String ROUTE = "api/v1/users";
 
     @GetMapping(value = ROUTE)
     public static ResponseBody getUser() {
@@ -18,7 +19,7 @@ public class UserController {
                 .Builder(UUID.randomUUID().toString())
                 .setUsername("branlix2000")
                 .setFullname("Noah Greff")
-                .setEmail("somthing@gmail.com")
+                .setEmail("something@gmail.com")
                 .setPhone("+31 06 41 53 14")
                 .build();
 
@@ -28,5 +29,12 @@ public class UserController {
                 .build();
 
         return response;
+    }
+
+    @GetMapping("/error")
+    public static ResponseBody error() {
+        return new ResponseBody.Builder(new Timestamp(System.currentTimeMillis()), 500)
+                .setData("Invalid URL path")
+                .build();
     }
 }
