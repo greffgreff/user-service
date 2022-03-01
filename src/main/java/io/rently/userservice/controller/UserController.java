9de8dd58b11dac8274b33378ff5bd2a200fcf8c8
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 18:20:33 +0100

    added request methods

--- src/main/java/io/rently/userservice/controller/UserController.java
+++ src/main/java/io/rently/userservice/controller/UserController.java
@@ -3,17 +3,24 @@ package io.rently.userservice.controller;
 import io.rently.userservice.dto.User;
 import io.rently.userservice.dto.ResponseBody;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.UUID;
 
 @RestController
 public class UserController implements ErrorController {
-    public static final String ROUTE = "api/v1/users";
+    public static final String PREFIX = "api/v1";
 
-    @GetMapping(value = ROUTE)
+    @RequestMapping("/error")
+    public static ResponseBody error() {
+        return new ResponseBody
+                .Builder(new Timestamp(System.currentTimeMillis()), 500)
+                .setData("Invalid or incomplete URL path")
+                .build();
+    }
+
+    @GetMapping(value = PREFIX + "/users")
     public static ResponseBody getUser() {
         Object data = new User
                 .Builder(UUID.randomUUID().toString())
@@ -31,10 +38,27 @@ public class UserController implements ErrorController {
         return response;
     }
 
-    @GetMapping("/error")
-    public static ResponseBody error() {
-        return new ResponseBody.Builder(new Timestamp(System.currentTimeMillis()), 500)
-                .setData("Invalid URL path")
+    @PostMapping(value = PREFIX + "/users")
+    public static ResponseBody addUser() {
+        return new ResponseBody
+                .Builder(new Timestamp(System.currentTimeMillis()), 201)
+                .setData("POST request")
+                .build();
+    }
+
+    @PutMapping(value = PREFIX + "/users/user")
+    public static ResponseBody updatedUser() {
+        return new ResponseBody
+                .Builder(new Timestamp(System.currentTimeMillis()), 201)
+                .setData("PUT request")
+                .build();
+    }
+
+    @DeleteMapping(value = PREFIX + "/users/user")
+    public static ResponseBody deleteUser() {
+        return new ResponseBody
+                .Builder(new Timestamp(System.currentTimeMillis()), 201)
+                .setData("DELETE request")
                 .build();
     }
 }
