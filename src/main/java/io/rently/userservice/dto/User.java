f3b65a47486a93ed412e338eb4e5973da45c68e8
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 17:07:14 +0100

    added builder to user

--- src/main/java/io/rently/userservice/dto/User.java
+++ src/main/java/io/rently/userservice/dto/User.java
@@ -4,19 +4,73 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class User {
+    @JsonProperty("id")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final String id;          // required
+
     @JsonProperty("username")
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String username;
+    private final String username;    // optional
 
     @JsonProperty("fullname")
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String fullname;
+    private final String fullname;    // optional
 
     @JsonProperty("email")
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String email;
+    private final String email;       // optional
 
     @JsonProperty("phone")
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String phone;
+    private final String phone;       // optional
+
+    private User(Builder builder) {
+        this.id = builder.id;
+        this.username = builder.username;
+        this.fullname = builder.fullname;
+        this.email = builder.email;
+        this.phone = builder.phone;
+    }
+
+    public static class Builder {
+        private final String id;
+        private String username;
+        private String fullname;
+        private String email;
+        private String phone;
+
+        public Builder(String id) {
+            this.id = id;
+        }
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setFullname(String fullname) {
+            this.fullname = fullname;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public User build() {
+            User user = new User(this);
+            validate();
+            return user;
+        }
+
+        private void validate() {
+            // validate
+        }
+    }
 }
