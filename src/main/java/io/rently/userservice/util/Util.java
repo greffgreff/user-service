34c7564cd568036b25c2c3c625094fae9e1dc170
BuildTools <unconfigured@null.spigotmc.org>
2022-03-02 21:26:00 +0100

    added getCurrentTs method

--- src/main/java/io/rently/userservice/util/Util.java
+++ src/main/java/io/rently/userservice/util/Util.java
@@ -1,8 +1,14 @@
 package io.rently.userservice.util;
 
+import java.sql.Timestamp;
+
 public class Util {
 
     public static <T> T getNonNull(T this_, T that_) {
         return this_ != null ? this_ : that_;
+    }
+
+    public static Timestamp getCurrentTs() {
+        return new Timestamp(System.currentTimeMillis());
     }
 }
