6f3c0679deaddf7b02c92efdc3d432111a4bd8a7
BuildTools <unconfigured@null.spigotmc.org>
2022-03-02 21:27:37 +0100

    made user mutable again

--- src/main/java/io/rently/userservice/_service/UserService.java
+++ src/main/java/io/rently/userservice/_service/UserService.java
@@ -6,16 +6,17 @@ import io.rently.userservice.error.ConflictException;
 import io.rently.userservice.error.NotFoundException;
 import io.rently.userservice.util.Util;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class UserService {
-    private static final List<User> users = new ArrayList<>(Arrays.asList(
-            new User.Builder("1").setUsername("branlix2000").setFullName("Noah Greff").setEmail("something@gmail.com").setPhone("+31 06 41 53 14").build(),
-            new User.Builder("2").setUsername("than00ber").setFullName("Chandler Greff").setEmail("something.other@yahoo.com").build(),
-            new User.Builder("3").setUsername("chew kok").setFullName("Chew Kok").setEmail("chew.kok@hotmail.com").build()
+    private static final List<User> users = new ArrayList(Arrays.asList(
+            new User("1", "branlix2000", "Noah Greff", "noahgreff@gmail.com", "+31 06 41 53 14", Util.getCurrentTs(), Util.getCurrentTs()),
+            new User("2", "then00ber", "Chandler Greff", "chandlegreff@gmail.com", "+31 56 41 84 27", Util.getCurrentTs(), Util.getCurrentTs()),
+            new User("3", "chew_kok", "Chew kok", "noahgreff@gmail.com", null, Util.getCurrentTs(), Util.getCurrentTs())
     ));
 
     public static ResponseContent returnUsers() {
@@ -32,6 +33,7 @@ public class UserService {
         for (User existingUser: users) {
             handleUniqueProperties(user, existingUser);
         }
+        user.refreshCreationDate();
         users.add(user);
         return new ResponseContent.Builder().setMessage("Successfully added user with ID { id: " + user.getId() + " }").build();
     }
@@ -44,20 +46,25 @@ public class UserService {
 
     public static ResponseContent replaceUserById(String id, User userData) {
         User oldUser = getUserById(id);
+
         for (User existingUser: users) {
             if (!Objects.equals(existingUser.getId(), id)) {
                 handleUniqueProperties(userData, existingUser);
             }
         }
-        users.remove(oldUser);
-        users.add(new User
-                .Builder(id)
+
+        User updatedUser = new User(id);
+        updatedUser
                 .setUsername(Util.getNonNull(userData.getUsername(), oldUser.getUsername()))
                 .setFullName(Util.getNonNull(userData.getFullName(), oldUser.getFullName()))
                 .setEmail(Util.getNonNull(userData.getEmail(), oldUser.getEmail()))
                 .setPhone(Util.getNonNull(userData.getPhone(), oldUser.getPhone()))
-                .build()
-        );
+                .setCreatedOn(oldUser.getCreatedOn())
+                .refreshUpdateDate();
+
+        users.remove(oldUser);
+        users.add(updatedUser);
+
         return new ResponseContent.Builder().setMessage("Successfully updated user with ID { id: " + id + " }").build();
     }
 
