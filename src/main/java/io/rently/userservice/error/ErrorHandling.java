cd9cfc2b6f8e8df94a157172384759ebc81a5805
BuildTools <unconfigured@null.spigotmc.org>
2022-03-02 11:07:58 +0100

    used getStatus method from HttpException class

--- src/main/java/io/rently/userservice/error/ErrorHandling.java
+++ src/main/java/io/rently/userservice/error/ErrorHandling.java
@@ -8,16 +8,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.sql.Timestamp;
-
 @ControllerAdvice
 public class ErrorHandling extends RuntimeException {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public static @ResponseBody ResponseContent handleRouteNotFound() {
         return new ResponseContent
-                .Builder(HttpStatus.NOT_FOUND)
+                .Builder(HttpStatus.BAD_REQUEST)
                 .setMessage("Invalid or incomplete URL path")
                 .build();
     }
@@ -26,7 +24,7 @@ public class ErrorHandling extends RuntimeException {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public static @ResponseBody ResponseContent handleNotFound(NotFoundException ex) {
         return new ResponseContent
-                .Builder(HttpStatus.NOT_FOUND)
+                .Builder(ex.getStatus())
                 .setMessage(ex.getMessage())
                 .build();
     }
@@ -35,7 +33,7 @@ public class ErrorHandling extends RuntimeException {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public static @ResponseBody ResponseContent handleUserNotFound(NotFoundException.UserByIdNotFound ex) {
         return new ResponseContent
-                .Builder(HttpStatus.NOT_FOUND)
+                .Builder(ex.getStatus())
                 .setMessage(ex.getMessage())
                 .build();
     }
