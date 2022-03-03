05f1dda186dcaa6a22b01389c0d138590c0989c6
BuildTools <unconfigured@null.spigotmc.org>
2022-03-03 20:39:01 +0100

    removed jsonable

--- src/main/java/io/rently/userservice/annotations/Jsonable.java
+++
@@ -1,7 +0,0 @@
-package io.rently.userservice.annotations;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-public @interface Jsonable {
-    JsonInclude include() default @JsonInclude(JsonInclude.Include.NON_NULL);
-}
