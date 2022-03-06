15dbcd2dfb4929dfa4b81b670bbc9fad4585cecc
chandle greff <unconfigured@null.spigotmc.org>
2022-03-06 18:02:40 +0100

    fixed invalid request error handling

--- src/main/java/io/rently/userservice/errors/ErrorController.java
+++ src/main/java/io/rently/userservice/errors/ErrorController.java
@@ -1,17 +1,31 @@
 package io.rently.userservice.errors;
 
 import io.rently.userservice.dtos.ResponseContent;
+import io.rently.userservice.errors.enums.Errors;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.servlet.http.HttpServletResponse;
 
-@ControllerAdvice
+@EnableWebMvc
+@RestControllerAdvice
 public class ErrorController {
 
     @ExceptionHandler(ResponseStatusException.class)
     public static ResponseContent handleResponseException(HttpServletResponse response, ResponseStatusException ex) {
         response.setStatus(ex.getStatus().value());
         return new ResponseContent.Builder(ex.getStatus()).setMessage(ex.getReason()).build();
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public static ResponseContent handleInvalidPath(HttpServletResponse response, NoHandlerFoundException ex) {
+        System.out.println(ex.getRequestURL());
+        ResponseStatusException respEx = Errors.INVALID_URL_PATH.getException();
+        response.setStatus(respEx.getStatus().value());
+        return new ResponseContent.Builder(respEx.getStatus()).setMessage(respEx.getReason()).build();
     }
 }
