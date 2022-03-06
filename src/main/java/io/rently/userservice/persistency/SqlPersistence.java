d3d79b8fcadd1a4f15e4cc53d281f992c92691b2
chandle greff <unconfigured@null.spigotmc.org>
2022-03-06 12:30:05 +0100

    added dedicated set methods for est con

--- src/main/java/io/rently/userservice/persistency/SqlPersistence.java
+++ src/main/java/io/rently/userservice/persistency/SqlPersistence.java
@@ -1,53 +1,80 @@
 package io.rently.userservice.persistency;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import io.rently.userservice.errors.HttpException;
+import io.rently.userservice.errors.enums.Errors;
 import io.rently.userservice.interfaces.IDatabaseContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
-//@Service
+@Controller
 public class SqlPersistence<T> implements IDatabaseContext<T> {
     private static final MysqlDataSource dataSource = new MysqlDataSource();
-    private static final Connection cnn;
+    private static Connection cnn;
 
-    static {
-        dataSource.setUser("dbi433816");
-        dataSource.setPassword("admin");
-        dataSource.setServerName("studmysql01.fhict.local");
-        dataSource.setDatabaseName("dbi433816");
-        try {
-            cnn = dataSource.getConnection();
-        }
-        catch (Exception exception) {
-            throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not connect to server");
-        }
+    @Autowired
+    public SqlPersistence() {
+        this.setUser("dbi433816")
+            .setPassword("admin")
+            .setServerName("studmysql01.fhict.local")
+            .setDatabaseName("dbi433816");
     }
 
-//    @Autowired
-    public SqlPersistence() {
-//        Statement stmt = cnn.createStatement();
-//        ResultSet rs = stmt.executeQuery("SELECT * FROM product");
-//        System.out.println(rs.findColumn("name"));
+    public SqlPersistence<T> setUser(String user) {
+        dataSource.setUser(user);
+        return this;
+    }
+
+    public SqlPersistence<T> setPassword(String password) {
+        dataSource.setPassword(password);
+        return this;
+    }
+
+    public SqlPersistence<T> setServerName(String server) {
+        dataSource.setServerName(server);
+        return this;
+    }
+
+    public SqlPersistence<T> setDatabaseName(String db) {
+        dataSource.setDatabaseName(db);
+        return this;
+    }
+
+    public void initConnection() {
+        throw Errors.DATABASE_CONNECTION_FAILED.getException();
+//
+//        try {
+//            cnn = dataSource.getConnection();
+//        }
+//        catch (SQLException ex) {
+//            System.out.println("SERVER ERROR");
+//            throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to establish connection to database: " + ex.getMessage());
+//        }
     }
 
     @Override
     public ArrayList<T> get() {
+        //        Statement stmt = cnn.createStatement();
+        //        ResultSet rs = stmt.executeQuery("SELECT * FROM product");
+        //        System.out.println(rs.findColumn("name"));
         return null;
     }
 
     @Override
     public void add(T obj) {
-
-    }
-
-    @Override
-    public void remove(T obj) {
-
+        try {
+            Statement sql = cnn.createStatement();
+        }
+        catch(SQLException ex) {
+//            throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
     }
 
     @Override
