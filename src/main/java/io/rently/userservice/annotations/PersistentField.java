72870823c9529a6f9d12c58166bfe61bcf6009bc
chandle greff <unconfigured@null.spigotmc.org>
2022-03-06 22:56:22 +0100

    renamed anno, moved anno

--- src/main/java/io/rently/userservice/annotations/PersistentField.java
+++ src/main/java/io/rently/userservice/annotations/PersistentField.java
@@ -1,4 +1,4 @@
-package io.rently.userservice.persistency.annotations;
+package io.rently.userservice.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface SqlField {
+public @interface PersistentField {
 
-    String columnName();
+    String name();
 }
--- src/main/java/io/rently/userservice/annotations/PersistentObject.java
+++ src/main/java/io/rently/userservice/annotations/PersistentObject.java
@@ -1,4 +1,4 @@
-package io.rently.userservice.persistency.annotations;
+package io.rently.userservice.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface SqlEntity {
+public @interface PersistentObject {
 
-    String tableName();
+    String name();
 }
