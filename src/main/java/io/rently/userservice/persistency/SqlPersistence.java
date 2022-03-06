88291b8fa068aa7f1b9900ce0d22cc1bf872bbed
chandle greff <unconfigured@null.spigotmc.org>
2022-03-06 16:28:45 +0100

    worked on sql persistence

--- src/main/java/io/rently/userservice/persistency/SqlPersistence.java
+++ src/main/java/io/rently/userservice/persistency/SqlPersistence.java
@@ -3,64 +3,57 @@ package io.rently.userservice.persistency;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import io.rently.userservice.errors.enums.Errors;
 import io.rently.userservice.interfaces.IDatabaseContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
-@Controller
-public class SqlPersistence<T> implements IDatabaseContext<T> {
+public class SqlPersistence implements IDatabaseContext {
     private static final MysqlDataSource dataSource = new MysqlDataSource();
     private static Connection cnn;
 
-    @Autowired
-    public SqlPersistence() {
-        this.setUser("dbi433816")
-            .setPassword("admin")
-            .setServerName("studmysql01.fhict.local")
-            .setDatabaseName("dbi433816");
+    public SqlPersistence(String user, String password, String server, String db) {
+        setUser(user);
+        setPassword(password);
+        setServerName(server);
+        setDatabaseName(db);
     }
 
-    public SqlPersistence<T> setUser(String user) {
+    public static void setUser(String user) {
         dataSource.setUser(user);
-        return this;
     }
 
-    public SqlPersistence<T> setPassword(String password) {
+    public static void setPassword(String password) {
         dataSource.setPassword(password);
-        return this;
     }
 
-    public SqlPersistence<T> setServerName(String server) {
+    public static void setServerName(String server) {
         dataSource.setServerName(server);
-        return this;
     }
 
-    public SqlPersistence<T> setDatabaseName(String db) {
+    public static void setDatabaseName(String db) {
         dataSource.setDatabaseName(db);
-        return this;
     }
 
-    public void initConnection() {
-        throw Errors.DATABASE_CONNECTION_FAILED.getException();
-//
-//        try {
-//            cnn = dataSource.getConnection();
-//        }
-//        catch (SQLException ex) {
-//            System.out.println("SERVER ERROR");
-//            throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to establish connection to database: " + ex.getMessage());
-//        }
+    private static void open() throws ResponseStatusException {
+        try {
+            cnn = dataSource.getConnection();
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            throw Errors.DATABASE_CONNECTION_FAILED.getException();
+        }
     }
+
+    private static void close() {
+        try {
+            cnn.close();
+        }
+        catch(Exception ignore) { }
+    } // placeholder method
 
     @Override
-    public ArrayList<T> get() {
+    public ArrayList<Object> get() {
         //        Statement stmt = cnn.createStatement();
         //        ResultSet rs = stmt.executeQuery("SELECT * FROM product");
         //        System.out.println(rs.findColumn("name"));
@@ -68,22 +61,15 @@ public class SqlPersistence<T> implements IDatabaseContext<T> {
     }
 
     @Override
-    public void add(T obj) {
-        try {
-            Statement sql = cnn.createStatement();
-        }
-        catch(SQLException ex) {
-//            throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
-        }
-    }
+    public void add(Object obj) {}
 
     @Override
-    public void update(T obj) {
+    public void update(Object obj) {
 
     }
 
     @Override
-    public void delete(T obj) {
+    public void delete(Object obj) {
 
     }
 }
