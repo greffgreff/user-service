8d5ba805ee2f49e23ffc85dd7c077e5007798a74
chandle greff <unconfigured@null.spigotmc.org>
2022-03-07 05:42:28 +0100

    added getObjectFromClass method to sql util

--- src/main/java/io/rently/userservice/persistency/SqlMapper.java
+++ src/main/java/io/rently/userservice/persistency/SqlMapper.java
@@ -1,37 +1,19 @@
 package io.rently.userservice.persistency;
 
 import io.rently.userservice.annotations.PersistentField;
-import io.rently.userservice.annotations.PersistentObject;
 import io.rently.userservice.errors.enums.Errors;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class SqlMapper {
 
-    private static Field ;
-
     // Make use of supplier instead of reflection
-    public static <T> T mapResultSetToObject(Class<T> dto, ResultSet resultSet) {
-        Constructor<T> dtoConstructor;
-        try {
-            dtoConstructor = dto.getConstructor();
-        }
-        catch(Exception ex) {
-            System.out.println(ex.getMessage());
-            throw Errors.INTERNAL_SERVER_ERROR.getException();
-        }
-
-        T persistentObj;
-        try {
-            persistentObj = dtoConstructor.newInstance();
-        }
-        catch(Exception ex) {
-            System.out.println(ex.getMessage());
-            throw Errors.INTERNAL_SERVER_ERROR.getException();
-        }
+    public static <T> T mapResultSetToObject(Class<T> dto, HashMap<String, String> data) {
+        T persistentObj = getObjectFromClass(dto);
 
         for (Field field: persistentObj.getClass().getDeclaredFields()) {
             PersistentField persistentField = field.getDeclaredAnnotation(PersistentField.class);
@@ -39,15 +21,47 @@ public class SqlMapper {
                 field.setAccessible(true);
 
                 try {
-                    field.set(persistentObj, resultSet.getString(persistentField.name()));
+                    for (Map.Entry<String, String> pair : data.entrySet()) {
+                        if (Objects.equals(pair.getKey(), persistentField.name())) {
+                            System.out.println(pair);
+                            field.set(persistentObj, pair.getValue());
+                        }
+                    }
                 }
                 catch (Exception ex) {
                     System.out.println(ex.getMessage());
-                    throw Errors.INTERNAL_SERVER_ERROR.getException();
+                    System.out.println("Setting field error");
+//                    throw Errors.INTERNAL_SERVER_ERROR.getException();
+                }
+                finally {
+                    try { field.set(persistentObj, null); }
+                    catch (Exception ignore) { }
                 }
             }
         }
 
         return persistentObj;
+    }
+
+    private static <T> T getObjectFromClass(Class<T> obj) {
+        Constructor<T> dtoConstructor;
+        try {
+            dtoConstructor = obj.getConstructor();
+        }
+        catch(Exception ex) {
+            System.out.println(ex.getMessage());
+            throw Errors.INTERNAL_SERVER_ERROR.getException();
+        }
+
+        T newObj;
+        try {
+            newObj = dtoConstructor.newInstance();
+        }
+        catch(Exception ex) {
+            System.out.println(ex.getMessage());
+            throw Errors.INTERNAL_SERVER_ERROR.getException();
+        }
+
+        return newObj;
     }
 }
