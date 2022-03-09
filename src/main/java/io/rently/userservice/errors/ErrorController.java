b6982e52459345e79a775efcb82cd4af999dcc7a
chandle greff <unconfigured@null.spigotmc.org>
2022-03-10 00:29:00 +0100

    added no method exception and invalid json format exception

--- src/main/java/io/rently/userservice/errors/ErrorController.java
+++ src/main/java/io/rently/userservice/errors/ErrorController.java
@@ -3,6 +3,7 @@ package io.rently.userservice.errors;
 import io.rently.userservice.dtos.ResponseContent;
 import io.rently.userservice.errors.enums.Errors;
 import io.rently.userservice.util.Broadcaster;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,6 +20,22 @@ public class ErrorController {
     public static ResponseContent handleResponseException(HttpServletResponse response, ResponseStatusException ex) {
         response.setStatus(ex.getStatus().value());
         return new ResponseContent.Builder(ex.getStatus()).setMessage(ex.getReason()).build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public static ResponseContent handleExceptions(HttpServletResponse response, Exception ex) {
+        ResponseStatusException respEx = Errors.INTERNAL_SERVER_ERROR.getException();
+        response.setStatus(respEx.getStatus().value());
+        Broadcaster.error("Internal server error occurred", ex);
+        return new ResponseContent.Builder(respEx.getStatus()).setMessage(respEx.getReason()).build();
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class) // caught before running service
+    public static ResponseContent handleInvalidFormatException(HttpServletResponse response, Exception ex) {
+        ResponseStatusException respEx = Errors.INVALID_DATA.getException();
+        response.setStatus(respEx.getStatus().value());
+        Broadcaster.info("Invalid data provided");
+        return new ResponseContent.Builder(respEx.getStatus()).setMessage(respEx.getReason()).build();
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
