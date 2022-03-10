33dfb89da0841318ed529ed40f8cc9c0c1fc06da
chandle greff <unconfigured@null.spigotmc.org>
2022-03-10 03:20:54 +0100

    added primary anno to config beams

--- src/test/java/io/rently/userservice/configs/TestConfigs.java
+++ src/test/java/io/rently/userservice/configs/TestConfigs.java
@@ -5,16 +5,19 @@ import io.rently.userservice.services.UserService;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @TestConfiguration
 public class TestConfigs {
 
     @Bean
+    @Primary
     public IDatabaseContext repositoryBeanTest() {
         return Mockito.mock(IDatabaseContext.class);
     }
 
     @Bean
+    @Primary
     public UserService userServiceBeanTest() {
         return Mockito.mock(UserService.class);
     }
